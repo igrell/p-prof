402f31efcdff37714469393e8aca26fbeb9f22d8
igrell <93985679+igrell@users.noreply.github.com>
2026-02-08 00:27:39 +0100

    08-02-2026 00:05:48

--- widgets/info_box.gd
+++ widgets/info_box.gd
@@ -12,15 +12,20 @@ func set_text(text, post_delay:float = 0.7) -> Tween:
 	tween_.interpolate_method(tween_, "nop", 0, 0, post_delay, 0, 0, 0.5)
 	return tween_.block()
 
+func _ready():
+	pass
+
 func set_text_for_confirm(text) -> Tween:
 	info_text_.percent_visible = 0
-	info_text_.bbcode_text = str(text)
+	info_text_.bbcode_text = text
 	tween_.interpolate_property(info_text_, "percent_visible", null, 1, 0.5)
 	tween_.step_property(arrow_, "visible", false, true, 0.5)
 	tween_.interpolate_method(tween_, "nop", 0, 0, 999999999, 0, 0, 0.5)
 	tween_.connect("tween_all_completed", arrow_, "set_visible", [false], CONNECT_ONESHOT | CONNECT_REFERENCE_COUNTED)
 	tween_.connect("tween_all_completed", $activate, "play", [], CONNECT_ONESHOT | CONNECT_REFERENCE_COUNTED)
 	return tween_.block()
+
+
 
 func clear_text() -> void:
 	arrow_.visible = false
